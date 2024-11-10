f33bcacc5437745beca6368aacd36067abac2ffe
GiyasRzza <giyassrzza@gmail.com>
2024-11-10 04:57:42 +0400

    training stanford nlp

--- src/main/java/com/nlpmodel/CVExtractor.java
+++ src/main/java/com/nlpmodel/CVExtractor.java
@@ -14,15 +14,16 @@ public class CVExtractor {
         for (CoreMap sentence : sentences) {
             for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
                 String word = token.get(CoreAnnotations.TextAnnotation.class);
+                String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
                 String ne = token.get(CoreAnnotations.NamedEntityTagAnnotation.class);
-
-                if (ne.equals("LOCATION")) {
-                    System.out.println("City: " + word);
-                } else if (ne.equals("ORGANIZATION")) {
-                    System.out.println("School ya da Organisation: " + word);
-                } else if (ne.equals("DEPARTMENT")) {
-                    System.out.println("Specialty: " + word);
+                System.out.println("Word: " + word + " POS: " + pos+" NE: " + ne);
+                switch (ne) {
+                    case "LOCATION", "STATE_OR_PROVINCE" -> System.out.println("\033[31mLocation: \033[0m" + word);
+                    case "ORGANIZATION" -> System.out.println("\033[32mOrganization: \033[0m" + word);
+                    case "PERSON" -> System.out.println("\033[34mPerson: \033[0m" + word);
+                    case "DEPARTMENT" -> System.out.println("\033[33mDepartment: \033[0m" + word);
                 }
+                System.out.println("---------------------------------------");
             }
         }
     }
@@ -32,10 +33,15 @@ public class CVExtractor {
         props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner");
         props.setProperty("ner.model", "models/custom-ner-model.ser.gz");
         StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-
-
-        String text = "John Doe completed his studies at Harvard University and currently works as a software engineer in New York. He previously lived in San Francisco.";
-
+//        String text= "I studied at Baku State University";
+//        String text= "I live in New York";
+        String text="Barack Obama, who was the 44th President of the United States, " +
+                "delivered a powerful speech at the United Nations " +
+                "Headquarters in New York City, " +
+                "where he discussed global issues including climate change, " +
+                "international security, " +
+                "and the importance of diplomacy in " +
+                "fostering peaceful relations between countries.";
         Annotation annotation = new Annotation(text);
         pipeline.annotate(annotation);
         return annotation;
--- src/main/java/com/nlpmodel/TrainNERModel.java
+++ src/main/java/com/nlpmodel/TrainNERModel.java
@@ -1,21 +1,21 @@
-package com.nlpmodel;
-
-import edu.stanford.nlp.ie.crf.CRFClassifier;
-import edu.stanford.nlp.util.CoreMap;
-import java.util.Properties;
-
-public class TrainNERModel {
-    public static void main(String[] args) {
-        String trainFile = "src/main/resources/data/training-data.txt";
-        String modelFile = "models/custom-ner-model.ser.gz";
-        Properties props = new Properties();
-        props.setProperty("trainFile", trainFile);
-        props.setProperty("serializeTo", modelFile);
-        props.setProperty("map", "word=0,answer=1");
-        props.setProperty("numIters", "100");
-        props.setProperty("useClassFeature", "true");
-        CRFClassifier<CoreMap> classifier = new CRFClassifier<>(props);
-        classifier.train();
-        System.out.println("The model was successfully trained and " + modelFile + " saved to file.");
-    }
-}
+//package com.nlpmodel;
+//
+//import edu.stanford.nlp.ie.crf.CRFClassifier;
+//import edu.stanford.nlp.util.CoreMap;
+//import java.util.Properties;
+//
+//public class TrainNERModel {
+//    public static void main(String[] args) {
+//        String trainFile = "src/main/resources/data/training-data.txt";
+////        String modelFile = "models/custom-ner-model.ser.gz";
+//        Properties props = new Properties();
+//        props.setProperty("trainFile", trainFile);
+//        props.setProperty("serializeTo", modelFile);
+//        props.setProperty("map", "word=0,answer=1");
+//        props.setProperty("numters", "5000");
+//        props.setProperty("useClassFeature", "true");
+//        CRFClassifier<CoreMap> classifier = new CRFClassifier<>(props);
+//        classifier.train();
+//        System.out.println("The model was successfully trained and " + modelFile + " saved to file.");
+//    }
+//}
